3b18d8633ec19e049bdf402442ef1a524ca5b2bd
钟运圣 <1643417491@qq.com>
2026-06-03 21:04:48 +0800

    ?

--- 钟运圣/exit.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "../declaration.h"
-
-void fun_8()
-{
-    printf("Hello World!");
-}
---
+++ 钟运圣/exit_.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../declaration.h"
+#include <stdlib.h>
+
+void exit_()
+{
+    printf("\n========================================\n");
+    printf("感谢使用学生成绩管理系统！\n");
+    printf("再见！\n");
+    printf("========================================\n");
+
+    exit(0);  // 正常退出程序，0 表示正常结束
+}
